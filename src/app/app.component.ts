63d7dce583184bec71118d69c151812646283923
RogoJuerg <juergen.rogozin@gmail.com>
2022-05-24 14:05:29 +0200

    Work on Slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'juergen.de';
+export class AppComponent implements OnInit {
+  images = ['pexels-dominika-roseclay-4148018.jpg', 'pexels-marcus-aurelius-6787953.jpg', 'pexels-marcus-aurelius-6787953.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
 
 
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
 
+      setTimeout(()=> {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
 
 
 }
