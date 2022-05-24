d8592ebafabbc52da5b899681e2b54fbc92a75da
RogoJuerg <juergen.rogozin@gmail.com>
2022-05-24 18:41:48 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    
-  }
+export class AppComponent{
 
-}
+  constructor(public router: Router) { }
+    
+
+  }
+    
